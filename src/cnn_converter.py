d2f0800ca772eab5fc351751313d5acbf69abc4a
David Baumgartner <2er0@dbaumi.at>
2018-05-12 22:58:14 +0200

    fixed issue with storing

--- src/cnn_converter.py
+++ src/cnn_converter.py
@@ -11,6 +11,9 @@ train_path_short = os.getcwd() + mainPath + 'train_short.csv'
 train_path_long = os.getcwd() + mainPath + 'train_long.csv'
 test_path = os.getcwd() + mainPath + 'test.csv'
 
+store_shape = (0, 60, 41, 2)
+label_shape = (0, 10)
+
 # # read csv mappings
 train = pd.read_csv(train_path_long)
 test = pd.read_csv(test_path)
@@ -18,24 +21,23 @@ test = pd.read_csv(test_path)
 # transform test data
 test_waves_compact = ut.extract_by_ids_cnn(test['ID'].values, wavPath)
 test_labels_short = ut.one_hot_encode_list(test['Class'].values)
-test_waves = np.ndarray((0, 60, 41, 2))
-test_labels = np.ndarray((0, 10))
+test_waves = np.ndarray(store_shape)
+test_labels = np.ndarray(label_shape)
 for v, l in zip(test_waves_compact, test_labels_short):
     if len(v) < 1:
         continue
     test_waves = np.append(test_waves, v, axis=0)
     test_labels = np.append(test_labels, [l for _ in range(len(v))], axis=0)
 
-# full_test_data.shape => (x, y, 60, 41, 2)
-# y = sub images
+# test_waves.shape => (x, 60, 41, 2)
 full_test_data = np.array(list(zip(test_waves, test_labels)))
 np.save(npPath + 'cnnTestData.npy', full_test_data)
 
 # transform train data
 train_waves_compact = ut.extract_by_ids_cnn(train['ID'].values, wavPath)
 train_labels_short = ut.one_hot_encode_list(train['Class'].values)
-train_waves = np.ndarray((0, 60, 41, 2))
-train_labels = np.ndarray((0, 10))
+train_waves = np.ndarray(store_shape)
+train_labels = np.ndarray(label_shape)
 for v, l in zip(train_waves_compact, train_labels_short):
     if len(v) < 1:
         continue
